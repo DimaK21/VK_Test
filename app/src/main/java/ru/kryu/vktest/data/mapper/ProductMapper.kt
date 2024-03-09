4758bb93c9e47997bc8f7e0f4730d25a5da77032
Dmitry Kryushnikov <civ02@mail.ru>
2024-03-09 22:23:24 +0500

    модель Goods и маппер

---
+++ app/src/main/java/ru/kryu/vktest/data/mapper/ProductMapper.kt
@@ -0,0 +1,13 @@
+package ru.kryu.vktest.data.mapper
+
+import ru.kryu.vktest.data.dto.Product
+import ru.kryu.vktest.domain.model.Goods
+
+object ProductMapper {
+    fun map(product: Product): Goods = Goods(
+        id = product.id,
+        title = product.title,
+        description = product.description,
+        thumbnail = product.thumbnail,
+    )
+}
---
+++ app/src/main/java/ru/kryu/vktest/domain/model/Goods.kt
@@ -0,0 +1,8 @@
+package ru.kryu.vktest.domain.model
+
+data class Goods(
+    val id: Int,
+    val title: String,
+    val description: String,
+    val thumbnail: String,
+)
