b1369030cb8891ce0d0e21d4839e902256966f1a
Dmitry Kryushnikov <civ02@mail.ru>
2024-03-09 20:57:45 +0500

    классы Resource и ErrorType

---
+++ app/src/main/java/ru/kryu/vktest/util/Resource.kt
@@ -0,0 +1,6 @@
+package ru.kryu.vktest.util
+
+sealed class Resource<T>(val data: T? = null, val errorType: ErrorType? = null) {
+    class Success<T>(data: T) : Resource<T>(data)
+    class Error<T>(errorType: ErrorType, data: T? = null) : Resource<T>(data, errorType)
+}
