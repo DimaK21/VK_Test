7a117996b595b102c84edf7224eca4f5f7774e29
Dmitry Kryushnikov <civ02@mail.ru>
2024-03-10 13:40:05 +0500

    di data

--- app/src/main/java/ru/kryu/vktest/app/App.kt
+++ app/src/main/java/ru/kryu/vktest/app/App.kt
@@ -1,10 +1,17 @@
 package ru.kryu.vktest.app
 
 import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+import ru.kryu.vktest.di.diModule
 
 class App: Application() {
 
     override fun onCreate() {
         super.onCreate()
+        startKoin {
+            androidContext(this@App)
+            modules(diModule)
+        }
     }
 }
---
+++ app/src/main/java/ru/kryu/vktest/di/DiModules.kt
@@ -0,0 +1,48 @@
+package ru.kryu.vktest.di
+
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import ru.kryu.vktest.data.NetworkClient
+import ru.kryu.vktest.data.network.ApiService
+import ru.kryu.vktest.data.network.RetrofitNetworkClient
+
+const val API_BASE_URL = "https://dummyjson.com/"
+
+val diModule = module {
+    includes(
+        dataModule,
+        repositoryModule,
+        useCaseModule,
+        viewModelModule,
+    )
+}
+
+val dataModule = module {
+    single<NetworkClient> {
+        RetrofitNetworkClient(
+            apiService = get(),
+            context = androidContext()
+        )
+    }
+    single<ApiService> {
+        Retrofit.Builder()
+            .baseUrl(API_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+    }
+}
+
+val repositoryModule = module {
+
+}
+
+val useCaseModule = module {
+
+}
+
+val viewModelModule = module {
+
+}
