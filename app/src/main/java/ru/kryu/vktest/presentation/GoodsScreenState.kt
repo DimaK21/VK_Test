1935bb8d05204474eb4e68acc2dac009030e2dae
Dmitry Kryushnikov <civ02@mail.ru>
2024-03-10 22:24:49 +0500

    изменение состояний в GoodsViewModel

--- app/src/main/java/ru/kryu/vktest/presentation/GoodsScreenState.kt
+++ app/src/main/java/ru/kryu/vktest/presentation/GoodsScreenState.kt
@@ -6,8 +6,5 @@ import ru.kryu.vktest.util.ErrorType
 sealed class GoodsScreenState {
     data object Loading : GoodsScreenState()
     data object Empty : GoodsScreenState()
-    data class Error(val error: ErrorType) : GoodsScreenState()
     data class Content(val content: List<Goods>) : GoodsScreenState()
-    data object LoadingNextPage : GoodsScreenState()
-    data class LoadingNextPageError(val error: ErrorType) : GoodsScreenState()
 }
